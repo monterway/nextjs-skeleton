38f69b2f054d10d554767ec1a45944b428bd55a7
Simeon Hristov <diwett5@gmail.com>
2023-06-30 23:31:45 +0300

    Added NextLink component

---
+++ web/src/components/NextLink/NextLink.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link';
+
+export interface NextLinkProps {
+  title: string | JSX.Element;
+  href: string;
+  className?: string;
+}
+
+const NextLink = (props: NextLinkProps): JSX.Element => {
+  const { title, href, className = '' } = props;
+
+  return (
+    <Link href={href}>
+      <a className={className}>
+        {title}
+      </a>
+    </Link>
+  );
+};
+
+export default NextLink;
