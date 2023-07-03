fbf4be65f895cd1be50fe7cec522acfebf7ac7b7
Simeon Hristov <diwett5@gmail.com>
2023-07-03 12:32:25 +0300

    Added translations option from database

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -2,13 +2,16 @@ import type { AppProps } from 'next/app';
 import React, { Fragment } from 'react';
 import Head from 'next/head';
 import PageLoader from '../src/components/PageLoader/PageLoader';
-import useTranslator from '../src/hooks/useTranslator';
+import useTranslator, { TranslationsType } from '../src/hooks/useTranslator';
 import TranslatorContext from '../src/contexts/TranslatorContext';
 import 'assets/scss/style.scss';
 import { UserType } from '../../types/UserType';
 import { onAuthStateChanged } from '@firebase/auth';
 import FirebaseAuth from '../src/services/Firebase/FirebaseAuth';
 import UserContext from '../src/contexts/UserContext';
+import FirebaseDb from '../src/services/Firebase/FirebaseDb';
+import { collection, getDocs } from 'firebase/firestore';
+import merge from 'deepmerge';
 
 const App = (props: AppProps) => {
   const { Component } = props;
@@ -16,6 +19,35 @@ const App = (props: AppProps) => {
   const [isAppLoaded, setIsAppLoaded] = React.useState<boolean>(false);
   const [theme, setTheme] = React.useState<string>('dark');
   const [user, setUser] = React.useState<UserType | null>(null);
+  const [firestoreTranslations, setFirestoreTranslations] = React.useState<TranslationsType>({});
+  const [translations, setTranslations] = React.useState<TranslationsType>({
+    en: {
+      test: 'TEST_EN'
+    },
+    de: {
+      test: 'TEST_DE'
+    }
+  });
+
+  React.useEffect(() => {
+    const collectionRef = collection(FirebaseDb, 'translations');
+    getDocs(collectionRef).then((querySnapshot) => {
+      const newFirestoreTranslations = querySnapshot.docs.reduce(
+        (newFirestoreTranslations, doc) => ({
+          ...newFirestoreTranslations,
+          [doc.id]: doc.data()
+        }),
+        {}
+      );
+      setFirestoreTranslations(newFirestoreTranslations);
+    });
+  }, []);
+
+  React.useEffect(() => {
+    setTranslations((translations) => ({
+      ...merge.all([translations, firestoreTranslations])
+    }));
+  }, [firestoreTranslations]);
 
   React.useEffect(() => {
     document.documentElement.setAttribute('data-bs-theme', theme);
@@ -39,14 +71,7 @@ const App = (props: AppProps) => {
   }, []);
 
   const { isLoaded: isTranslatorLoaded, translator } = useTranslator({
-    translations: {
-      en: {
-        test: 'TEST_EN'
-      },
-      de: {
-        test: 'TEST_DE'
-      }
-    }
+    translations
   });
 
   const isLoading = !isAppLoaded || !isTranslatorLoaded;
