3137b41dcfd008d80c05ca719d70e390f4bcb538
Simeon Hristov <diwett5@gmail.com>
2023-07-21 23:46:55 +0300

    Update _app.tsx

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -95,6 +95,7 @@ const App = (props: AppProps) => {
             <Head>
               <meta charSet="utf-8" />
               <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+              <link rel="icon" type="image/x-icon" href="/favicon.svg" />
               {Seo.getTitleTag()}
               {Seo.getDescriptionTag()}
             </Head>
