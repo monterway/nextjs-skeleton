f679be43168b952bed598502e08d67537f494caf
Simeon Hristov <diwett5@gmail.com>
2023-07-22 20:34:02 +0300

    Added invalid function call handling

--- web/src/core/modules/Functions/Functions.ts
+++ web/src/core/modules/Functions/Functions.ts
@@ -1,7 +1,8 @@
-import { ResponseType } from '../../../../../types/ResponseType';
-import { httpsCallable } from '@firebase/functions';
+import {ResponseType} from '../../../../../types/ResponseType';
+import {httpsCallable} from '@firebase/functions';
 import FirebaseFunctions from '../../services/Firebase/FirebaseFunctions';
-import { HttpError } from './HttpError';
+import {HttpError} from './HttpError';
+import {FunctionsError} from "./FunctionsError";
 
 export interface FunctionsType {
   call: (path: string, data?: any) => Promise<ResponseType>;
@@ -9,13 +10,23 @@ export interface FunctionsType {
 
 const Functions = (): FunctionsType => ({
   call: async (path, data = {}) => {
-    const callable = httpsCallable(FirebaseFunctions, `app/${path}`);
-    const result = await callable(data);
-    const response = result.data as ResponseType;
-    if (response.status === 'OK') {
-      return response;
-    } else {
-      throw new HttpError(response);
+    try {
+      const callable = httpsCallable(FirebaseFunctions, `app/${path}`);
+      const result = await callable(data);
+      const response = result.data as ResponseType;
+      if (response.status === 'OK') {
+        return response;
+      } else {
+        throw new HttpError(response);
+      }
+    }
+    catch (error: any) {
+      if (error instanceof HttpError) {
+        throw error;
+      }
+      else {
+        throw new FunctionsError();
+      }
     }
   }
 });
---
+++ web/src/core/modules/Functions/FunctionsError.ts
@@ -0,0 +1,5 @@
+export class FunctionsError extends Error {
+  constructor() {
+    super("FUNCTIONS_ERROR");
+  }
+}
