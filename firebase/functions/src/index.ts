2e9f99eef00ced47b530da3e9f7042f1ad8ec0fd
Simeon Hristov <diwett5@gmail.com>
2023-07-23 23:28:35 +0300

    Disabled CORS

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,7 +1,7 @@
 import * as admin from "firebase-admin";
 import * as express from "express";
 import * as functions from "firebase-functions";
-import * as cors from "cors";
+// import * as cors from "cors";
 import * as cookieParser from "cookie-parser";
 import AuthModule from "./core/modules/Auth/Auth";
 import AuthRoutes from "./core/routes/Auth/Auth";
@@ -10,10 +10,10 @@ import AppRoutes from "./app/routes/App";
 admin.initializeApp();
 
 const app = express();
-const appCors = cors({origin: true});
+// const appCors = cors({origin: true});
 const appCookieParser = cookieParser();
 
-app.use(appCors);
+// app.use(appCors);
 app.use(appCookieParser);
 app.use(AuthModule().setUserInRequest);
 
