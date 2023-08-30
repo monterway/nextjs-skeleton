d701e0aaa2a7245901614b2735051bb94bd7b02a
Simeon Hristov <diwett5@gmail.com>
2023-08-30 14:25:00 +0300

    Fixed bug with general folder

--- web/src/core/contexts/UserContext.ts
+++ web/src/core/contexts/UserContext.ts
@@ -1,5 +1,5 @@
 import React from 'react';
-import { UserType } from '../../../../types/UserType';
+import { UserType } from '../../../../general/types/UserType';
 
 const UserContext = React.createContext<UserType | null>(null);
 
