c9582b7738e272a653f92717c03a7953323f63ac
Simeon Hristov <diwett5@gmail.com>
2023-10-22 13:44:20 +0300

    Create AddToOrderButton.tsx

---
+++ web/src/core/components/AddToOrderButton/AddToOrderButton.tsx
@@ -0,0 +1,43 @@
+import {Button} from "react-bootstrap";
+import React from "react";
+import OrderHandlerContext from "../../contexts/OrderHandlerContext";
+import {ButtonProps} from "react-bootstrap/Button";
+
+export interface AddToOrderButtonProps {
+  productId: string;
+  quantity: number;
+  children: JSX.Element | JSX.Element[];
+  buttonProps?: ButtonProps;
+}
+
+const AddToOrderButton = (props: AddToOrderButtonProps): JSX.Element | null => {
+  const {
+    productId,
+    quantity,
+    children,
+    buttonProps = {},
+  } = props;
+  const orderHandler = React.useContext(OrderHandlerContext);
+
+  const addToOrder = () => {
+    orderHandler.set((order) => {
+      order.productOrders.push({
+        id: new Date().getTime().toString(),
+        productId,
+        quantity,
+      });
+      return order;
+    });
+  };
+
+  return (
+    <Button
+      onClick={addToOrder}
+      {...buttonProps}
+    >
+      { children }
+    </Button>
+  );
+}
+
+export default AddToOrderButton;
